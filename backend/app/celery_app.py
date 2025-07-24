0f88218d0cfea28c65df355733e1ef5af8e335a1
Roman Prudnikov <romanprudnikov1990@gmail.com>
2025-07-25 00:21:47 +0300

    fix celery

--- backend/app/celery_app.py
+++ backend/app/celery_app.py
@@ -1,8 +1,11 @@
 from celery import Celery
 import os
 
+
 celery_app = Celery(
     "worker",
     broker=os.getenv("REDIS_URL", "redis://redis:6379/0"),
     backend=os.getenv("REDIS_URL", "redis://redis:6379/0")
 )
+
+celery_app.autodiscover_tasks(['app']) 
