ace4a5ac90d3f3484f63fa2f5ccf2230295166fc
Roman Prudnikov <romanprudnikov1990@gmail.com>
2025-07-25 01:20:17 +0300

    fix configs

--- backend/app/services/config.py
+++ backend/app/services/config.py
@@ -2,8 +2,4 @@ import os
 
 
 UPLOAD_DIR = os.getenv("UPLOAD_DIR", "/tmp")
-TRITON_HOST = os.getenv("TRITON_HOST", "http://triton:8000")
-LLM_HOST = os.getenv("LLM_HOST", "http://llm:8000")
-LLAMA_MODEL_PATH = os.getenv("LLAMA_MODEL_PATH", "/models/llama-1b.Q4_K_M.gguf")
-USE_LLAMA_CPP = os.getenv("USE_LLAMA_CPP", "true").lower() == "true"
 USE_FASTER_WHISPER = os.getenv("USE_FASTER_WHISPER", "true").lower() == "true"
--- backend/app/services/whisper_client.py
+++ backend/app/services/whisper_client.py
@@ -1,15 +1,9 @@
-from app.services.config import USE_FASTER_WHISPER, TRITON_HOST
-import requests
+from app.services.config import USE_FASTER_WHISPER
 
 if USE_FASTER_WHISPER:
     from faster_whisper import WhisperModel
     model = WhisperModel("base", compute_type="int8")  # можно поменять на "tiny", "small" и т.п.
 
 def transcribe(path: str) -> str:
-    if USE_FASTER_WHISPER:
-        segments, _ = model.transcribe(path, language="ru")
-        return " ".join([seg.text for seg in segments])
-    else:
-        with open(path, "rb") as f:
-            response = requests.post(f"{TRITON_HOST}/transcribe", files={"audio_file": f})
-        return response.json().get("text", "")
+    segments, _ = model.transcribe(path, language="ru")
+    return " ".join([seg.text for seg in segments])
